e989a06cc8ebd67d4953ec4556271211d91f1652
zerock <kangckck24@gmail.com>
2025-03-31 14:22:08 +0900

    insert 50

--- src/test/java/org/zerock/sb2/todo/TodoRepoTests.java
+++ src/test/java/org/zerock/sb2/todo/TodoRepoTests.java
@@ -4,29 +4,70 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.sb2.todo.entities.Todo;
 import org.zerock.sb2.todo.repository.TodoRepository;
+
 
 import lombok.extern.log4j.Log4j2;
 
 @SpringBootTest
 @Log4j2
+@Transactional
 public class TodoRepoTests {
   
   @Autowired(required = false)
   private TodoRepository repository;
 
 
+  //gradlew build -x test
+  //@Disabled
   @Test
+  @Commit
   public void testInsert(){
     //insert는 save( )하시면 끝 
 
-    Todo todo = Todo.builder()
-    .title("Test")
-    .writer("user1")
-    .build();
+    for(int i = 0; i < 50 ; i++){
+      Todo todo = Todo.builder()
+      .title("Test")
+      .writer("user1")
+      .build();
 
-    repository.save(todo);
+      repository.save(todo);
+    }
   }
+
+  @Test
+  public void testRead(){
+
+    java.util.Optional<Todo> result = repository.findById(1L);
+
+    log.info(result.get());
+
+  }
+
+  @Test
+  public void testDelete() {
+
+    Long tno = 1L;
+
+    repository.deleteById(tno);
+
+  }
+
+  @Test
+  @Commit
+  public void testUpdate() {
+
+    java.util.Optional<Todo> result = repository.findById(2L);
+
+    Todo todo = result.get();
+
+    todo.changeTitle("Changed Title 234----1");
+
+  }
+
+
 
 }
