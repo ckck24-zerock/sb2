50e2ad938101cd81b7195ebadab85f3660704e39
zerock <kangckck24@gmail.com>
2025-04-01 11:22:17 +0900

    ddd

--- src/test/java/org/zerock/sb2/board/BoardRepoTests.java
+++ src/test/java/org/zerock/sb2/board/BoardRepoTests.java
@@ -1,18 +1,16 @@
 package org.zerock.sb2.board;
 
-import static org.mockito.ArgumentMatchers.isNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.sb2.board.entities.BoardEntity;
 import org.zerock.sb2.board.repository.BoardRepository;
 
 import lombok.extern.log4j.Log4j2;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) 
+@SpringBootTest
 @Log4j2
 public class BoardRepoTests {
 
