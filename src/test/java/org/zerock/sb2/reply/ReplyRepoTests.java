89dcec353286b3475b09ffd1af0680265021d181
zerock <kangckck24@gmail.com>
2025-04-02 13:37:13 +0900

    ddd

--- src/test/java/org/zerock/sb2/reply/ReplyRepoTests.java
+++ src/test/java/org/zerock/sb2/reply/ReplyRepoTests.java
@@ -1,7 +1,10 @@
 package org.zerock.sb2.reply;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.sb2.board.entities.BoardEntity;
+import org.zerock.sb2.reply.entities.ReplyEntity;
 import org.zerock.sb2.reply.repository.ReplyRepository;
 
 import lombok.extern.log4j.Log4j2;
@@ -14,5 +17,24 @@ public class ReplyRepoTests {
   
   @Autowired(required = false)
   private ReplyRepository repository;
+
+  @Test
+  public void testInsert() {
+
+    //가짜 BoardEntity 필요 
+    BoardEntity board = BoardEntity.builder().bno(123L).build();
+
+    ReplyEntity replyEntity = ReplyEntity.builder()
+    .replyText("댓글입니다. 댓글입니다.")
+    .replyer("user00")
+    .board(board)
+    .build();
+
+    repository.save(replyEntity);
+
+    
+
+  }
+
 
 }
