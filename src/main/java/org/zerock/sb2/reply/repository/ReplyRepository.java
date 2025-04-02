13090d3a703da846f14fad03607ddcc73e83c031
zerock <kangckck24@gmail.com>
2025-04-02 14:42:21 +0900

    ffefe

--- src/main/java/org/zerock/sb2/reply/repository/ReplyRepository.java
+++ src/main/java/org/zerock/sb2/reply/repository/ReplyRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.zerock.sb2.reply.dto.ReplyListDTO;
 import org.zerock.sb2.reply.entities.ReplyEntity;
 
 public interface ReplyRepository extends JpaRepository<ReplyEntity, Long>{
@@ -15,4 +16,7 @@ public interface ReplyRepository extends JpaRepository<ReplyEntity, Long>{
 
   @Query("select r.rno, r.replyText, r.replyer, r.board.bno , r.regDate, r.modDate from ReplyEntity r where r.board.bno = :bno ")
   Page<Object[]> listOfBoard2(@Param("bno") Long bno, Pageable pageable );
+
+  @Query("select new org.zerock.sb2.board.dto.ReplyListDTO(r.rno, r.replyText, r.replyer, r.board.bno , r.regDate, r.modDate) from ReplyEntity r where r.board.bno = :bno ")
+  Page<ReplyListDTO> listOfBoard3(@Param("bno") Long bno, Pageable pageable );
 }
