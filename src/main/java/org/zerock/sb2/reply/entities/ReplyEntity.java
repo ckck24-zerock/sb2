8d9bd7922049b0d35edb600c3eeaffba46a347b6
zerock <kangckck24@gmail.com>
2025-04-02 13:10:17 +0900

    ddd

---
+++ src/main/java/org/zerock/sb2/reply/entities/ReplyEntity.java
@@ -0,0 +1,32 @@
+package org.zerock.sb2.reply.entities;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@EntityListeners(value = AuditingEntityListener.class)
+@Entity
+@Table(name = "tbl_reply")
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReplyEntity {
+  
+}
