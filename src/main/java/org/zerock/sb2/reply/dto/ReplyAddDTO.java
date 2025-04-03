952ba2f405cce5e5d44ef147f517c5fe1044fc73
zerock <kangckck24@gmail.com>
2025-04-03 15:31:31 +0900

    service exception 2

--- src/main/java/org/zerock/sb2/reply/dto/ReplyAddDTO.java
+++ src/main/java/org/zerock/sb2/reply/dto/ReplyAddDTO.java
@@ -1,6 +1,7 @@
 package org.zerock.sb2.reply.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,6 +20,6 @@ public class ReplyAddDTO {
     @NotBlank
     private String replyer;
 
-    @NotBlank
+    @NotNull
     private Long bno;
 }
