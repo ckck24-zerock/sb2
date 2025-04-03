07488d9592ef362076e4467fd31b23faacf0c1ac
zerock <kangckck24@gmail.com>
2025-04-03 13:29:20 +0900

    addefe

--- src/main/java/org/zerock/sb2/reply/dto/ReplyAddDTO.java
+++ src/main/java/org/zerock/sb2/reply/dto/ReplyAddDTO.java
@@ -1,4 +1,20 @@
 package org.zerock.sb2.reply.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+
+
+@Data
+
 public class ReplyAddDTO {
+
+    @NotBlank
+    private String replyText;
+
+    @NotBlank
+    private String replyer;
+
+    @NotBlank
+    private Long bno;
 }
--- src/main/java/org/zerock/sb2/reply/service/ReplyService.java
+++ src/main/java/org/zerock/sb2/reply/service/ReplyService.java
@@ -1,4 +1,20 @@
 package org.zerock.sb2.reply.service;
 
+import org.zerock.sb2.board.entities.BoardEntity;
+import org.zerock.sb2.reply.dto.ReplyAddDTO;
+import org.zerock.sb2.reply.entities.ReplyEntity;
+
 public interface ReplyService {
+
+    Long add(ReplyAddDTO addDTO);
+
+    default ReplyEntity dtoToEntity(ReplyAddDTO addDTO){
+
+        return ReplyEntity.builder()
+                .replyText(addDTO.getReplyText())
+                .replyer(addDTO.getReplyer())
+                .board(BoardEntity.builder().bno(addDTO.getBno()).build())
+                .build();
+    }
+
 }
