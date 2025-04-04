4223de464f43160b26995baab661063677a5209f
zerock <kangckck24@gmail.com>
2025-04-04 15:17:03 +0900

    product test

--- src/main/java/org/zerock/sb2/product/repository/ProductSearchImpl.java
+++ src/main/java/org/zerock/sb2/product/repository/ProductSearchImpl.java
@@ -14,6 +14,8 @@ import org.zerock.sb2.product.entities.ProductEntity;
 import org.zerock.sb2.product.entities.QProductEntity;
 import org.zerock.sb2.product.entities.QProductImage;
 
+import java.util.List;
+
 @Log4j2
 @RequiredArgsConstructor
 public class ProductSearchImpl implements ProductSearch {
@@ -39,7 +41,7 @@ public class ProductSearchImpl implements ProductSearch {
         query.offset(pageRequestDTO.getOffset());
         query.orderBy(new OrderSpecifier<>(Order.DESC, qProductEntity.pno));
 
-        query.select(Projections.bean(
+        JPQLQuery<ProductListDTO> dtoQuery = query.select(Projections.bean(
                 ProductListDTO.class,
                 qProductEntity.pno,
                 qProductEntity.pname,
@@ -47,7 +49,14 @@ public class ProductSearchImpl implements ProductSearch {
                 qProductImage.imgName.as("imgName")
                 ));
 
-        return null;
+        List<ProductListDTO> dtoList = dtoQuery.fetch();
+        long total = dtoQuery.fetchCount();
+
+        return PageResponseDTO.<ProductListDTO>withAll()
+                .dtoList(dtoList)
+                .total((int) total)
+                .pageRequestDTO(pageRequestDTO)
+                .build();
     }
 }
 
