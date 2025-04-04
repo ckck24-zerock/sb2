6e31d2a030bedafc9b2ea2095b53a842227ca100
zerock <kangckck24@gmail.com>
2025-04-04 09:57:28 +0900

    service exception 2

--- src/main/java/org/zerock/sb2/board/entities/BoardEntity.java
+++ src/main/java/org/zerock/sb2/board/entities/BoardEntity.java
@@ -1,23 +1,19 @@
 package org.zerock.sb2.board.entities;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
+import jakarta.persistence.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.zerock.sb2.category.entities.CategoryEntity;
 
 @EntityListeners(value = AuditingEntityListener.class)
 @Entity
@@ -45,6 +41,8 @@ public class BoardEntity {
   private boolean delFlag;
 
   private int viewCnt;
+
+
   
   @CreatedDate
   @Column(name = "regdate", updatable = false)
