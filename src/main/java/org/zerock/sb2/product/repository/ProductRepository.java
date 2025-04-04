0b5c200f914230923e4d809125f0d3a86254cb4a
zerock <kangckck24@gmail.com>
2025-04-04 14:10:34 +0900

    product test

--- src/main/java/org/zerock/sb2/product/repository/ProductRepository.java
+++ src/main/java/org/zerock/sb2/product/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package org.zerock.sb2.product.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.zerock.sb2.board.dto.PageRequestDTO;
 import org.zerock.sb2.product.dto.ProductReadDTO;
 import org.zerock.sb2.product.entities.ProductEntity;
 
@@ -12,6 +15,11 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     @EntityGraph(attributePaths = "images", type = EntityGraph.EntityGraphType.FETCH)
     @Query("select p from ProductEntity p where p.pno = :pno ")
     ProductEntity selectOne(@Param("pno") Long pno);
+
+    //pno, pname, price, imgName
+    @Query("select p.pno, p.pname, p.price " +
+            "from ProductEntity p ")
+    Page<Object[]> list1(Pageable pageable);
 
 
 }
