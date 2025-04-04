7bf49b6cf6986f65f61814750c7073d57f3cc4e5
zerock <kangckck24@gmail.com>
2025-04-04 14:20:15 +0900

    product test

--- src/main/java/org/zerock/sb2/product/repository/ProductRepository.java
+++ src/main/java/org/zerock/sb2/product/repository/ProductRepository.java
@@ -17,7 +17,7 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     ProductEntity selectOne(@Param("pno") Long pno);
 
     //pno, pname, price, imgName
-    @Query("select p.pno, p.pname, p.price " +
+    @Query("select p.pno, p.pname, p.price, pi.imgName " +
             "from ProductEntity p left join p.images pi WHERE pi.ord = 0 ")
     Page<Object[]> list1(Pageable pageable);
 
