8a09988ebaea3b95c6fde407a549f1b676c049ba
zerock <kangckck24@gmail.com>
2025-04-04 14:16:47 +0900

    product test

--- src/test/java/org/zerock/sb2/product/ProductRepoTests.java
+++ src/test/java/org/zerock/sb2/product/ProductRepoTests.java
@@ -4,6 +4,10 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.sb2.product.dto.ProductReadDTO;
 import org.zerock.sb2.product.entities.ProductEntity;
 import org.zerock.sb2.product.repository.ProductRepository;
@@ -66,6 +70,15 @@ public class ProductRepoTests {
         ProductReadDTO dto = new ProductReadDTO(product);
 
         log.info(dto);
+    }
+
+    @Test
+    public void testList1() {
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("pno").descending());
+
+        Page<Object[]> result = repo.list1(pageable);
+
     }
 }
 
